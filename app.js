861c2eac11569495c76c6fac3a58418814612d85
Anubhav2324 <kumaranubhav8844@gmail.com>
2022-06-07 17:19:21 +0530

    Added Styles

--- app.js
+++ app.js
@@ -1,14 +1,20 @@
- setInterval(()=>{
-    d = new Date;
-    hvalue = d.getHours();
-    mvalue = d.getMinutes();
-    svalue = d.getSeconds();
-    hrotation = 30*hvalue + mvalue/2;
-    mrotation = 6*mvalue;
-    srotation = 6*svalue;
-    hour.style.transform = `rotate(${hrotation}deg)`;
-    minute.style.transform = `rotate(${mrotation}deg)`;
-    second.style.transform = `rotate(${srotation}deg)`;
- },1000)
+setInterval(myClock,1000);
+
+ function myClock (){
+   d = new Date;
+   hvalue = d.getHours();
+   mvalue = d.getMinutes();
+   svalue = d.getSeconds();
+   hrotation = 30*hvalue + mvalue/2;
+   mrotation = 6*mvalue;
+   srotation = 6*svalue;
+   hour.style.transform = `rotate(${hrotation}deg)`;
+   minute.style.transform = `rotate(${mrotation}deg)`;
+   second.style.transform = `rotate(${srotation}deg)`;
+};
+
+
+
+
  
 
